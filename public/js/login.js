1a925e710f3223ba6694f4809cbfb6d19cfdc6ca
Francisco Dias <xicodias1999@gmail.com>
2022-07-06 15:11:52 +0100

    Prepared app for deployment

--- public/js/login.js
+++ public/js/login.js
@@ -3,11 +3,10 @@ import axios from 'axios';
 import { showAlert } from './alerts';
 
 export const login = async (email, password) => {
-  console.log(email, password);
   try {
     const res = await axios({
       method: 'POST',
-      url: 'http://127.0.0.1:3000/api/v1/users/login',
+      url: '/api/v1/users/login',
       data: {
         email,
         password,
@@ -20,8 +19,6 @@ export const login = async (email, password) => {
         location.assign('/');
       }, 1500);
     }
-
-    console.log(res);
   } catch (err) {
     showAlert('error', err.response.data.message);
   }
@@ -31,7 +28,7 @@ export const logout = async () => {
   try {
     const res = await axios({
       method: 'GET',
-      url: 'http://127.0.0.1:3000/api/v1/users/logout',
+      url: '/api/v1/users/logout',
     });
 
     if ((res.data.status = 'success')) location.reload(true);
